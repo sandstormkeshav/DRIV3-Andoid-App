8359e574abc320e5b94feac7d2e998a477896d09
Keshav Sreekumar <sandstormkeshav@gmail.com>
2014-10-26 18:47:17 -0700

    The App used in DRIV3 to map locations on a map
    Work In Progress

---
+++ src/com/example/myapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapp;
+
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends android.support.v4.app.FragmentActivity 
+implements OnMapClickListener {
+
+	private GoogleMap mMap;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// Try to obtain the map from the SupportMapFragment.
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+        mMap.setOnMapClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onMapClick(LatLng position) {
+		
+		mMap.addMarker(new MarkerOptions()
+							.position(position).
+							icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+		
+	}
+
+}
